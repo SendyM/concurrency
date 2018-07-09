f4dcbd2112e7bdaec9edefcda605d998096a7cdd
Sendy <daishihua@sanjiang365.com>
2018-07-09 16:46:11 +0800

    移除不必要的类

--- src/main/java/com/mmall/concurrency/example/atomic/AtomicExample2.java
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample2.java
@@ -5,7 +5,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 
